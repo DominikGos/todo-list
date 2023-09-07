3cd79ca4df543c0f823d7a83542c1380681e4622
Dominik <dominikgos3@gmail.com>
2023-09-08 00:20:36 +0200

    fixed response in 'userTasks' method of TaskController

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -8,14 +8,12 @@ use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
-use function Termwind\render;
-
 class TaskController extends Controller
 {
     public function userTasks(int $userId): Response
     {
-        //add authorization 
-        $tasks = User::findOrFail($userId)->tasks();
+        //add authorization
+        $tasks = User::findOrFail($userId)->tasks;
 
         return Inertia::render('users/tasks', [
             'tasks' => $tasks
--- resources/js/Pages/users/tasks.vue
+++ resources/js/Pages/users/tasks.vue
@@ -1,5 +1,5 @@
 <script setup>
-
+const props = defineProps({tasks: Array})
 </script>
 
 <template>
