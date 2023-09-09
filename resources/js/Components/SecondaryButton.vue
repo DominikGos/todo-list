fc57ab4dce581bbb82846d75c43f830028d23711
Dominik <dominikgos3@gmail.com>
2023-09-09 15:51:08 +0200

    added secondary button and success button

---
+++ resources/js/Components/SecondaryButton.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'button'
+  }
+})
+</script>
+
+<template>
+  <button :type="props.type" class="bg-gray-400 p-2 rounded-md text-white focus:ring-2 focus:ring-gray-400 hover:cursor-pointer hover:bg-gray-300 transition-all">
+    <slot></slot>
+  </button>
+</template>
---
+++ resources/js/Components/SuccessButton.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'button'
+  }
+})
+</script>
+
+<template>
+  <button :type="props.type" class="bg-emerald-400 p-2 rounded-md text-white focus:ring-2 focus:bg-emerald-400 hover:cursor-pointer hover:bg-emerald-300 transition-all">
+    <slot></slot>
+  </button>
+</template>
