b1ff5353d54b06ddd73d271ffa282eb1750cbe0b
Dominik <dominikgos3@gmail.com>
2023-09-11 17:16:22 +0200

    added modal component

---
+++ resources/js/Components/Modal.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { computed } from 'vue'
+
+defineEmits(['closeModal'])
+
+const props = defineProps({
+  active: {
+    type: Boolean,
+    default: false,
+  }
+})
+
+const backgroundClasses = computed(() => {
+  return [
+    props.active 
+      ? 'backdrop-brightness-50 visible opacity-100'
+      : 'backdrop-brightness-100  invisible opacity-0' 
+      , ' mx-auto p-3 w-screen h-screen fixed top-0 right-0 left left-0 transition-all duration-500 '
+  ] 
+})
+
+const bodyClasses = computed(() => {
+  return [
+    props.active 
+      ? 'translate-y-0 opacity-100'
+      : 'translate-y-6 opacity-0'
+      , 'mt-5 bg-white rounded-md shadow-md p-3 transition-all duration-500'
+  ] 
+})
+
+</script>
+
+<template>
+  <div :class="backgroundClasses">
+    <div :class="bodyClasses">
+      <div class="flex justify-end">
+        <button @click="$emit('closeModal')"><i class="fa-solid fa-x"></i></button>
+      </div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
