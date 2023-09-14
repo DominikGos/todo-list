8676f2890c98fddb507297315347bc214cf7c1f5
Dominik <dominikgos3@gmail.com>
2023-09-14 23:56:09 +0200

    added accessor in Task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -18,5 +20,16 @@ class Task extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    protected function createdAt(): Attribute
+    {
+        return Attribute::make(
+            get: function(string $value) {
+                $createdAt = new Carbon($value);
+                
+                return $createdAt->format('Y-m-d H:i');
+            } 
+        );
     }
 }
