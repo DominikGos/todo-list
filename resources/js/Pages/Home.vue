aa5625c6e7f4d47f7f7d2b4d75480b943b982938
Dominik <dominikgos3@gmail.com>
2023-09-16 14:26:57 +0200

    added home page content

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,10 +1,34 @@
 <script setup>
+import PrimaryButton from '../Components/PrimaryButton.vue';
 import MainLayout from '../Layouts/MainLayout.vue'
+import { Link } from '@inertiajs/vue3'
 
-defineOptions({layout: MainLayout})
+defineOptions({ layout: MainLayout })
 </script>
 
 <template>
-
-  home
+  <div class="container mx-auto p-3 mt-3">
+    <header>
+      <h3 class="text-2xl font-medium text-center">
+        Welcome to Your Todo List.
+      </h3>
+      <p class="text-gray-500 text-center font-medium">Organize Your Life, One Task at a Time</p>
+    </header>
+    <main class="mt-10">
+      <div v-if="$page.props.auth.user" class="flex justify-center gap-3 items-center">
+        <p class="text-xl">Create new task</p>
+        <Link :href="route('users.tasks.index', { id: $page.props.auth.user.id })">
+          <PrimaryButton>
+            Create
+          </PrimaryButton>
+        </Link>
+      </div>
+      <div v-else class="flex justify-center gap-3 items-center">
+        <p class="text-xl">Log into app</p>
+        <Link :href="route('login.page')">
+          <PrimaryButton> Login</PrimaryButton>
+        </Link>
+      </div>
+    </main>
+  </div>
 </template>
